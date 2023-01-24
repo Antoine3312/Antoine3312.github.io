ee9fc7518dfec3a4d4df4af5e8214f22a61b0ae8
MAZEAU Antoine <antoine.mazeau31@gmail.com>
2023-01-25 00:32:09 +0100

    Début page des choix

---
+++ choixprojet.js
@@ -0,0 +1,10 @@
+
+
+const choix1 = document.getElementsByClassName('choix')[0];
+const choix2 = document.getElementsByClassName('choix')[1];
+const choix3 = document.getElementsByClassName('choix')[2];
+const choix4 = document.getElementsByClassName('choix')[3];
+
+choix1.addEventListener('click', (e) => {
+    console.log('test');
+}) 
