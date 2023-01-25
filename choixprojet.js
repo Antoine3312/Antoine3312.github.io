6c9bbdb767c2f5905c6ef98a60eee07ef8ece787
MAZEAU Antoine <antoine.mazeau31@gmail.com>
2023-01-25 23:35:35 +0100

    choix projet

--- choixprojet.js
+++ choixprojet.js
@@ -5,9 +5,10 @@ const choix2 = document.getElementsByClassName('choix')[1];
 const choix3 = document.getElementsByClassName('choix')[2];
 const choix4 = document.getElementsByClassName('choix')[3];
 
-var h1Elements = document.getElementsByTagName("h1");
-var h2Elements = document.getElementsByTagName("h2");
-var discoveryCardElements = document.getElementsByClassName("discover_card");
+const h1Elements = document.getElementsByTagName("h1");
+const h2Elements = document.getElementsByTagName("h2");
+const discoveryCardElements = document.getElementsByClassName("discover_card");
+const menuElements= document.getElementsByClassName("menu");
 
 
 choix1.addEventListener('click', (e) => {
@@ -18,13 +19,9 @@ choix1.addEventListener('click', (e) => {
     choix4.style.flex = "1";
     for(var i = 0; i < h1Elements.length; i++) {
         if(i!=0) {
-            retireMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='hidden';
-            discoveryCardElements[i].style.visibility='hidden';
+            retireMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         } else {
-            afficheMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='visible';
-            discoveryCardElements[i].style.visibility='visible';
+            afficheMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         }
     }  
 }) 
@@ -37,14 +34,9 @@ choix2.addEventListener('click', (e) => {
     choix4.style.flex = "1";
     for(var i = 0; i < h1Elements.length; i++) {
         if(i!=1) {
-            retireMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='hidden';
-            discoveryCardElements[i].style.visibility='hidden';
+            retireMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         } else {
-
-            afficheMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='visible';
-            discoveryCardElements[i].style.visibility='visible';
+            afficheMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         }
     }
 }) 
@@ -56,13 +48,9 @@ choix3.addEventListener('click', (e) => {
     choix4.style.flex = "1";
     for(var i = 0; i < h1Elements.length; i++) {
         if(i!=2) {
-            retireMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='hidden';
-            discoveryCardElements[i].style.visibility='hidden';
+            retireMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         } else {
-            afficheMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='visible';
-            discoveryCardElements[i].style.visibility='visible';
+            afficheMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         }
     }
 
@@ -76,13 +64,9 @@ choix4.addEventListener('click', (e) => {
     choix1.style.flex = "1";
     for(var i = 0; i < h1Elements.length; i++) {
         if(i!=3) {
-            retireMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='hidden';
-            discoveryCardElements[i].style.visibility='hidden';
+            retireMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         } else {
-            afficheMenuH1(h1Elements[i]);
-            h2Elements[i].style.visibility='visible';
-            discoveryCardElements[i].style.visibility='visible';
+            afficheMenuH1(h1Elements[i], h2Elements[i], discoveryCardElements[i], menuElements[i]);
         }
     }
 
@@ -90,19 +74,32 @@ choix4.addEventListener('click', (e) => {
 }) 
 
 
-function retireMenuH1(choix){
-    choix.style.fontSize = "2vmax";
+function retireMenuH1(titre1, titre2, discover_card, menu){
+    titre1.style.fontSize = "2vmax";
     if (window.matchMedia("(min-width: 1250px)").matches) {
-        choix.style.writingMode = "vertical-rl";
-        choix.style.left = "40%";
+        titre1.style.writingMode = "vertical-rl";
+        titre1.style.left = "40%";
     }
-    choix.style.top = "40%";
+    titre1.style.top = "40%";
+
+    titre2.style.opacity='0';
     
+    discover_card.style.opacity='0';
+    discover_card.style.visibility = "hidden";
+
+    menu.style.visibility = 'hidden';
 }
 
-function afficheMenuH1(choix){
-    choix.style.fontSize = "4vmax";
-    choix.style.writingMode = "horizontal-tb";
-    choix.style.top = "7%";
-    choix.style.left = "5%";
+function afficheMenuH1(titre1, titre2, discover_card, menu){
+    titre1.style.fontSize = "4vmax";
+    titre1.style.writingMode = "horizontal-tb";
+    titre1.style.top = "7%";
+    titre1.style.left = "5%";
+
+    titre2.style.opacity='1';
+    
+    discover_card.style.opacity='1';
+    discover_card.style.visibility = "visible";
+
+    menu.style.visibility = 'visible';
 }
