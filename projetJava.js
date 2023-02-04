56cfe3c109ae4dcf45dc3b29d2c4400e037151f9
MAZEAU Antoine <antoine.mazeau31@gmail.com>
2023-02-04 13:20:25 +0100

    fin projet Java

--- projetJava.js
+++ projetJava.js
@@ -11,3 +11,55 @@ const observer = new IntersectionObserver((entries) =>{
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const slide1 = document.getElementsByClassName('slide-1')[0];
+const slide2 = document.getElementsByClassName('slide-2')[0];
+const slide3 = document.getElementsByClassName('slide-3')[0];
+const slide4 = document.getElementsByClassName('slide-4')[0];
+const slides = [slide1, slide2, slide3, slide4]
+
+const btnChoix1 = document.getElementsByClassName('choix1')[0];
+const btnChoix2 = document.getElementsByClassName('choix2')[0];
+const btnChoix3 = document.getElementsByClassName('choix3')[0];
+const btnChoix4 = document.getElementsByClassName('choix4')[0];
+const slideElement = document.getElementsByClassName('slide');
+
+
+var positions = [0, 100, 200, 300]
+var elementActif = 0
+
+console.log(slide1)
+
+// document.styleSheets[0].cssRules[0].style.removeProperty("transform");
+// slide1.style.transform = '';
+// slide1.style.transform = "translateX(-200%);"
+
+
+
+btnChoix1.addEventListener('click', (e) => {
+    updateCarousselle(slide1);
+});
+btnChoix2.addEventListener('click', (e) => {
+    updateCarousselle(slide2);
+});
+btnChoix3.addEventListener('click', (e) => {
+    updateCarousselle(slide3);
+});
+btnChoix4.addEventListener('click', (e) => {
+    updateCarousselle(slide4);
+});
+
+
+function updateCarousselle(slideChoisi){
+
+    gsap.to(".slide", {
+        x : () => -slideChoisi.offsetWidth*slides.indexOf(slideChoisi)-10,
+        duration : .5
+    });
+
+    setTimeout(function() {
+        elementActif = slides.indexOf(slideChoisi);
+    }, 500);
+    
+}
--- projetPython.js
+++ projetPython.js
@@ -1,55 +1,75 @@
-const tl = gsap.timeline({paused: true});
-console.log(tl)
+// const tl = gsap.timeline({paused: true});
+// console.log(tl)
 
-const animateOpenNav = () => {
-    tl.to(".nav-container", 0.2, {
-        autoAlpha: 1,
-        delay: 0.1,
-    });
+// const animateOpenNav = () => {
+//     tl.to(".nav-container", 0.2, {
+//         autoAlpha: 1,
+//         delay: 0.1,
+//     });
 
-    tl.to(
-        ".site-logo",
-        0.2,
-        {
-            color: "#fff",
-        },
-        "-=0.1"
-    );
-};
+//     tl.to(
+//         ".site-logo",
+//         0.2,
+//         {
+//             color: "#fff",
+//         },
+//         "-=0.1"
+//     );
+// };
 
-tl.from(".flex > div", 0.4, {
-    opacity: 0,
-    y: 10,
-    stagger: {
-        amount: 0.04,
-    },
+// tl.from(".flex > div", 0.4, {
+//     opacity: 0,
+//     y: 10,
+//     stagger: {
+//         amount: 0.04,
+//     },
+// });
+
+// tl.to(".nav-link > a",0.8, {
+//     top: 0,
+//     ease: "power2.inOut",
+//     stagger: {
+//         amount: 0.1
+//     }
+// }, "-=0.4");
+
+// tl.from(".nav-footer", 0.3, {
+//     opacity: 0
+// }, "-=0.5").reverse();
+
+
+// const openNav = () => {
+//     animateOpenNav();
+//     const navBtn = document.getElementsByClassName("menu-toggle-btn")[0];
+//     const element = document.querySelector(".menu-toggle-btn");
+//     element.style.setProperty("--check-primary", "#000")
+//     console.log(navBtn)
+//     navBtn.onclick = function(e){
+//         navBtn.classList.toggle("active");
+//         tl.reversed(!tl.reversed());
+//         element.style.setProperty("--check-primary", "#fff")
+//         console.log("test")
+//     };
+// };
+
+// openNav();
+
+
+let slider = document.querySelector('.slider');
+console.log(slider);
+
+gsap.to(".slider", {
+    x : () => slider.offsetWidth,
+    duration : 5
 });
 
-tl.to(".nav-link > a",0.8, {
-    top: 0,
-    ease: "power2.inOut",
-    stagger: {
-        amount: 0.1
-    }
-}, "-=0.4");
-
-tl.from(".nav-footer", 0.3, {
-    opacity: 0
-}, "-=0.5").reverse();
+gsap.to(".slider",{
+    x : 0,
+    duration : 5,
+    delay : 5
+})
 
 
-const openNav = () => {
-    animateOpenNav();
-    const navBtn = document.getElementsByClassName("menu-toggle-btn")[0];
-    const element = document.querySelector(".menu-toggle-btn");
-    element.style.setProperty("--check-primary", "#000")
-    console.log(navBtn)
-    navBtn.onclick = function(e){
-        navBtn.classList.toggle("active");
-        tl.reversed(!tl.reversed());
-        element.style.setProperty("--check-primary", "#fff")
-        console.log("test")
-    };
-};
 
-openNav();
+
+
